4f6fa367f4163f6db58f947371a92475d1267ee4
Giancarlo <giancarlolmile@gmail.com>
2025-05-30 09:20:09 -0500

    update

--- src/main/java/com/mitocode/product_service/controller/ProductoController.java
+++ src/main/java/com/mitocode/product_service/controller/ProductoController.java
@@ -1,16 +1,29 @@
 package com.mitocode.product_service.controller;
 
 import com.mitocode.product_service.entity.Producto;
+import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/product")
 public class ProductoController {
 
-    /*@GetMapping
-    public Producto getProducto (@PathVariable("id")){
-        return new Producto.builder().id(1L).build;
-    }*/
+    @GetMapping("/name")
+    public String getName() {
+        return "productName";
+    }
+
+    @GetMapping("/helloWorld")
+    public String helloWorld() {
+        return "Hello world from Service A!";
+    }
+
+
+    /*@FeignClient(name="PRODUCT-SERVICE", url="http://PRODUCT-SERVICE/product/name")
+    public interface String getName();*/
+
 
 }
