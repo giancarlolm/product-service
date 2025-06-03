447c0dac4b99692704dee0e975d913ece2220dcf
Giancarlo <giancarlolmile@gmail.com>
2025-06-02 23:38:33 -0500

    cambios de la clase 7

--- src/main/java/com/mitocode/product_service/controller/ProductoController.java
+++ src/main/java/com/mitocode/product_service/controller/ProductoController.java
@@ -1,6 +1,9 @@
 package com.mitocode.product_service.controller;
 
 import com.mitocode.product_service.entity.Producto;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.context.WebServerApplicationContext;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,6 +13,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/product")
 public class ProductoController {
+    // Inyectamos el contexto que sabe el WebServer y su puerto
+    private final WebServerApplicationContext webServerAppCtxt;
+
+    public ProductoController(WebServerApplicationContext webServerAppCtxt) {
+        this.webServerAppCtxt = webServerAppCtxt;
+    }
 
     @GetMapping("/name")
     public String getName() {
@@ -21,9 +30,10 @@ public class ProductoController {
         return "Hello world from Service A!";
     }
 
-
-    /*@FeignClient(name="PRODUCT-SERVICE", url="http://PRODUCT-SERVICE/product/name")
-    public interface String getName();*/
+    @GetMapping("hola")
+    public String Hola() {
+        return "Hola desde Producto " + webServerAppCtxt.getWebServer().getPort();
+    }
 
 
 }
